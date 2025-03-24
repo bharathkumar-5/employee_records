e2db30d298a5490e1de665ab1a199e37eb0106e9
Bharath Kumar <bharathmarley103@gmail.com>
2025-03-24 11:40:58 +0530

    Update index.js

--- Backend_emp/index.js
+++ Backend_emp/index.js
@@ -35,7 +35,10 @@ import argon2 from "argon2";
 
 const app=express();
 app.use(express.json())
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:8080', 'https://employee-records-beta.vercel.app']
+  }));
 app.use("/auth",authrouter)
 app.use("/emp",empRouter)
 
